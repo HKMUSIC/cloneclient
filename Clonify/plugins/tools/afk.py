74f756a854970c1894004c95606fc48e34619e47
HKMUSIC <himanshu4hk9@gmail.com>
2025-11-01 00:59:56 +0530

    Update afk.py

--- Clonify/plugins/tools/afk.py
+++ Clonify/plugins/tools/afk.py
@@ -4,7 +4,7 @@ from pyrogram.enums import MessageEntityType
 from pyrogram import filters
 from pyrogram.types import Message
 from Clonify import app
-from Clonify.mongo.readable_time import get_readable_time
+from Clonify.core.readable_time import get_readable_time
 from Clonify.mongo.afkdb import add_afk, is_afk, remove_afk
 
 @app.on_message(filters.command(["afk", "brb"], prefixes=["/", "!"]))
